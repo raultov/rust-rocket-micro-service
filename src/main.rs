334b972c2113c40e077ff76a344eb8460be41ca4
raultov <angel.raul.tovar.martin@gmail.com>
2021-07-29 10:59:27 +0200

    Cassandra retrieve row

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 #[macro_use] extern crate rocket;
 
+extern crate uuid;
+
+use scylla::{SessionBuilder, Session};
+use scylla::macros::FromRow;
+use scylla::frame::response::cql_to_rust::FromRow;
+use scylla::IntoTypedRows;
+use scylla::transport::retry_policy::DefaultRetryPolicy;
+
+use uuid::Uuid;
+
 use rocket::Request;
+use rocket::State;
 use rocket::serde::json::{Json, Value, json};
 use rocket::serde::{Serialize, Deserialize};
 
 #[get("/hello")]
-fn hello() -> Value {
+async fn hello() -> Value {
   json!({
     "status": "success",
     "message": "Hello API!"
@@ -20,7 +31,7 @@ struct Book {
 }
 
 #[post("/book", format = "application/json", data = "<book>")]
-fn new_book(book: Json<Book>) -> Value {
+async fn new_book(book: Json<Book>) -> Value {
     let mut dummy_db: Vec<&Book> = Vec::new();
     let new_book = book.into_inner();
     dummy_db.push(&new_book);
@@ -33,9 +44,50 @@ fn not_found(req: &Request) -> String {
     format!("Oh no! We couldn't find the requested path '{}'", req.uri())
 }
 
-#[launch]
-fn rocket() -> _ {
+#[derive(FromRow)]
+struct VehicleName {
+    name: String
+}
+
+#[get("/vehicle")]
+async fn get_vehicle(session: &State<Session>) -> Value {
+
+    let mut name: String = "unknown".to_string();
+    let user_id = Uuid::parse_str("d13fe953-297a-4781-807a-f9becc1b71f6").unwrap();
+    let vehicle_id = Uuid::parse_str("60e18f00-34b8-4a52-916c-adbb0204618e").unwrap();
+
+    if let Some(rows) = session.query("SELECT name FROM vehicles.vehicle WHERE user_id = ? and vehicle_id = ?", (user_id, vehicle_id))
+        .await
+        .expect("Failed to execute query")
+        .rows {
+            for row in rows.into_typed::<VehicleName>() {
+                let vehicle_name: VehicleName = row.expect("Failed to extract VehicleName from Row");
+                name = vehicle_name.name;
+            }
+        }
+
+  json!({
+    "vehicle_id": vehicle_id.to_string(),
+    "name": name
+  })
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+
+    let node = "nuckito:9042";
+
+    let session: Session = SessionBuilder::new()
+        .known_node(node)
+        .retry_policy(Box::new(DefaultRetryPolicy::new()))
+        .build()
+        .await
+        .expect(&format!("Failed to connect {}", node));
+
     rocket::build()
-    .register("/", catchers![not_found])
-    .mount("/api", routes![hello, new_book])
+      .register("/", catchers![not_found])
+      .mount("/api", routes![get_vehicle, hello, new_book])
+      .manage(session)
+      .launch()
+      .await
 }
