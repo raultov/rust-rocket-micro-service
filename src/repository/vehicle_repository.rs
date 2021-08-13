0f28cd4bab72cddf6ad9fa2765d79aa68f4623f7
raultov <angel.raul.tovar.martin@gmail.com>
2021-08-13 11:11:47 +0200

    implementing VehicleRepository unit tests

--- src/repository/vehicle_repository.rs
+++ src/repository/vehicle_repository.rs
@@ -1,7 +1,6 @@
 use cfg_if::cfg_if;
 
 use scylla::IntoTypedRows;
-use scylla::transport::retry_policy::DefaultRetryPolicy;
 use scylla::query::Query;
 
 use tokio_retry::Retry;
@@ -10,6 +9,8 @@ use tokio_retry::strategy::{ExponentialBackoff, jitter};
 use rocket::serde::uuid::Uuid;
 
 use crate::domain::vehicle::Vehicle;
+
+const SELECT_QUERY: &str = "SELECT name FROM vehicles.vehicle WHERE user_id = ? and vehicle_id = ?";
 
 pub struct VehicleRepository {
     session: Session
@@ -31,13 +32,12 @@ impl VehicleRepository {
             .take(3);    // limit to 3 retries
 
         let result = Retry::spawn(retry_strategy, || {
-            let mut get_vehicle_query: Query = Query::new("SELECT name FROM vehicles.vehicle WHERE user_id = ? and vehicle_id = ?".to_string());
-            get_vehicle_query.set_retry_policy(Box::new(DefaultRetryPolicy::new()));
+            let get_vehicle_query: Query = Query::new(SELECT_QUERY.to_string());
             self.session.query(get_vehicle_query, (user_id, vehicle_id))
         }).await;
 
         if let Some(rows) = result
-            .expect("Failed to execute query")
+            .expect("Failed to execute query") // TODO return Result instead of failing here
             .rows {
                 for row in rows.into_typed::<Vehicle>() {
                     let vehicle_name: Vehicle = row.expect("Failed to extract VehicleName from Row");
@@ -82,6 +82,7 @@ cfg_if! {
 #[cfg(test)]
 pub mod tests {
     use super::*;
+    use std::panic::catch_unwind;
     use scylla::QueryResult;
     use scylla::transport::errors::QueryError;
     use scylla::query::Query;
@@ -104,16 +105,91 @@ pub mod tests {
     }
 
     #[test]
-    fn given_user_id_and_vehicle_id_when_get_vehicle_name_then_returns_vehicle_name() {
+    fn when_new_then_returns_vehicle_repository() {
+        let vehicle_repository = aw!(VehicleRepository::new("node"));
+
+        //assert!(vehicle_repository.session.expect());
+    }
+
+    #[test]
+    fn when_get_vehicle_name_then_returns_vehicle_name() {
         let mut vehicle_repository = aw!(VehicleRepository::new("node"));
 
-        let _get_vehicle_query: Query = Query::new(fixture::QUERY_STR.to_string());
         let user_id = Uuid::parse_str(fixture::USER_ID_STR).unwrap();
         let vehicle_id = Uuid::parse_str(fixture::VEHICLE_ID_STR).unwrap();
 
-        vehicle_repository.session.expect_query().returning(move |_get_vehicle_query, (_user_id, _vehicle_id)| fixture::forge_query_result());
+        vehicle_repository.session.expect_query()
+            .withf(|query: &Query, _| query.get_contents() == SELECT_QUERY.to_string())
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.0.to_string() == fixture::USER_ID_STR)
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.1.to_string() == fixture::VEHICLE_ID_STR)
+            .times(1)
+            .returning(move |_, _| fixture::forge_query_result());
 
-        let vehicle_name = aw!(vehicle_repository.get_vehicle_name(user_id, vehicle_id)).expect("shit happens");
+        let vehicle_name = aw!(vehicle_repository.get_vehicle_name(user_id, vehicle_id)).unwrap();
+
+        assert_eq!(fixture::EXPECTED_VEHICLE_NAME, vehicle_name.name);
+    }
+
+    #[test]
+    fn given_no_matching_row_when_get_vehicle_name_then_returns_none() {
+        let mut vehicle_repository = aw!(VehicleRepository::new("node"));
+
+        let user_id = Uuid::parse_str(fixture::USER_ID_STR).unwrap();
+        let vehicle_id = Uuid::parse_str(fixture::VEHICLE_ID_STR).unwrap();
+
+        vehicle_repository.session.expect_query()
+            .withf(|query: &Query, _| query.get_contents() == SELECT_QUERY.to_string())
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.0.to_string() == fixture::USER_ID_STR)
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.1.to_string() == fixture::VEHICLE_ID_STR)
+            .times(1)
+            .returning(move |_, _| Ok(QueryResult::default()));
+
+        let vehicle_name = aw!(vehicle_repository.get_vehicle_name(user_id, vehicle_id));
+
+        assert!(vehicle_name.is_none());
+    }
+
+    #[test]
+    fn given_error_when_get_vehicle_name_and_no_matching_row_then_retries_up_to_4_times_then_panics() {
+        let mut vehicle_repository = aw!(VehicleRepository::new("node"));
+
+        let user_id = Uuid::parse_str(fixture::USER_ID_STR).unwrap();
+        let vehicle_id = Uuid::parse_str(fixture::VEHICLE_ID_STR).unwrap();
+
+        vehicle_repository.session.expect_query()
+            .withf(|query: &Query, _| query.get_contents() == SELECT_QUERY.to_string())
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.0.to_string() == fixture::USER_ID_STR)
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.1.to_string() == fixture::VEHICLE_ID_STR)
+            .times(4)
+            .returning(move |_, _| Err(QueryError::InvalidMessage("error".to_owned())));
+
+        let result = catch_unwind(|| aw!(vehicle_repository.get_vehicle_name(user_id, vehicle_id)));
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn given_single_error_when_get_vehicle_name_then_retries_and_returns_vehicle_name() {
+        let mut vehicle_repository = aw!(VehicleRepository::new("node"));
+
+        let user_id = Uuid::parse_str(fixture::USER_ID_STR).unwrap();
+        let vehicle_id = Uuid::parse_str(fixture::VEHICLE_ID_STR).unwrap();
+
+        vehicle_repository.session.expect_query()
+            .withf(|query: &Query, _| query.get_contents() == SELECT_QUERY.to_string())
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.0.to_string() == fixture::USER_ID_STR)
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.1.to_string() == fixture::VEHICLE_ID_STR)
+            .times(1)
+            .returning(move |_, _| Err(QueryError::InvalidMessage("error".to_owned())));
+
+        vehicle_repository.session.expect_query()
+            .withf(|query: &Query, _| query.get_contents() == SELECT_QUERY.to_string())
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.0.to_string() == fixture::USER_ID_STR)
+            .withf(|_, tuple: &(Uuid, Uuid)| tuple.1.to_string() == fixture::VEHICLE_ID_STR)
+            .times(1)
+            .returning(move |_, _| fixture::forge_query_result());
+
+        let vehicle_name = aw!(vehicle_repository.get_vehicle_name(user_id, vehicle_id)).unwrap();
 
         assert_eq!(fixture::EXPECTED_VEHICLE_NAME, vehicle_name.name);
     }
@@ -121,7 +197,6 @@ pub mod tests {
     mod fixture {
         use super::*;
 
-        pub const QUERY_STR: &str = "SELECT";
         pub const USER_ID_STR: &str = "a906615e-2e6a-4edb-9377-5a6b8544791b";
         pub const VEHICLE_ID_STR: &str = "88573010-cf4c-490e-9d29-f8517dc60b90";
         pub const EXPECTED_VEHICLE_NAME: &str = "the vehicle name";
