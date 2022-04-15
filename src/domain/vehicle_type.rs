c94f1360a1a58edb5d795302f0932b84af02a784
raultov <angel.raul.tovar.martin@gmail.com>
2022-04-15 08:27:37 +0200

    clean code

--- src/domain/vehicle_type.rs
+++
@@ -1,8 +0,0 @@
-use rocket::serde::{Serialize, Deserialize};
-use scylla::macros::{FromUserType, IntoUserType};
-use scylla::cql_to_rust::FromCqlVal;
-
-#[derive(Serialize, Deserialize, Debug, IntoUserType, FromUserType)]
-pub enum VehicleType {
-    bike
-}
